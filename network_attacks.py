ab55dd52a9eaa105a600d0e7fa775630defea8ba
elligalata <lougalata@gmail.com>
2020-05-30 17:54:44 +0300

    Rename random attacks to failures Create targeted attacks Plotting clustering distributions

--- network_attacks.py
+++ network_attacks.py
@@ -35,11 +35,14 @@ def get_attack_metrics(net):
     # Calculate max path length between nodes and the rest of the graph
     if nx.is_connected(net):
         nodes_eccentricity = np.array(list(nx.eccentricity(net).values()), dtype=np.int32)
-        cluster_sizes = [1.0]
+        avg_size_isolated_clusters = 1.0
+        relative_size_largest_cluster = 1.0
     else:
         # Find the separated insels network nodes
         components = sorted(nx.connected_components(net), key=len, reverse=True)
         cluster_sizes = [len(c) / len(nodes) for c in components]
+        avg_size_isolated_clusters = np.mean([len(x) for x in components[1:]])
+        relative_size_largest_cluster = cluster_sizes[0]
         # Consider eccentricitie of largest
         largest_component = components[0]
         largest_subgraph = net.subgraph(largest_component)
@@ -47,12 +50,14 @@ def get_attack_metrics(net):
 
     results["shortest_paths"] = shortest_path_list
     results["eccentricities"] = nodes_eccentricity
-    results["cluster_sizes"] = cluster_sizes
+
+    # Save a tuple of (S, <s>) which is the relative size of the largest cluster and the average size of the isolated clusters
+    results["cluster_sizes"] = (relative_size_largest_cluster, avg_size_isolated_clusters)
 
     return results
 
 
-def incremental_random_attack(net, removal_rate, max_rate=0.5, verbose=False):
+def incremental_random_failure(net, removal_rate, max_rate=0.5, verbose=False):
     """
     This type of attack simulates a sequential and time increasing attack, where at each iteration a constant ratio
     of the original net nodes are removed.
@@ -69,7 +74,7 @@ def incremental_random_attack(net, removal_rate, max_rate=0.5, verbose=False):
     min_path, max_path, cluster_size_ratios = None, None, []
 
     steps = int(max_rate / removal_rate)
-    for ratio_removed in tqdm([removal_rate] * steps, desc="- Incremental_attack", disable=not verbose, file=sys.stdout):
+    for ratio_removed in tqdm([removal_rate] * steps, desc="- Incremental_failure", disable=not verbose, file=sys.stdout):
         # Get list of nodes
         nodes = list(attacked_net.nodes)
         # Randomly select nodes to remove
@@ -93,7 +98,7 @@ def incremental_random_attack(net, removal_rate, max_rate=0.5, verbose=False):
     return min_path, max_path, cluster_size_ratios
 
 
-def instantaneous_random_attack(net, removal_rates, verbose=False):
+def instantaneous_random_failure(net, removal_rates, verbose=False):
     """
     This type of attack simulates an instantaneous attack where at each step the original network losses a randomly
     selected portion of its nodes. Each step is independent from the other.
@@ -132,6 +137,97 @@ def instantaneous_random_attack(net, removal_rates, verbose=False):
 
     return min_path, max_path, cluster_size_ratios
 
+
+def instantaneous_attack(net, removal_rates, verbose=False):
+    """
+    This type of attack simulates an instantaneous attack where at each step first remove the most connected node, and then
+    selecting and removing nodes in decreasing order of their connectivity k.
+    :param net: Network to attack
+    :param removal_rates: Vector of removal rates to test
+    :return: - shortest path [25% quantile, mean, 75% quantile]
+             - shortest path [25% quantile, mean, 75% quantile]
+             - cluster_sizes_ratios: Relative size of the clusters/islands of networks generated after the attack.
+    """
+
+    original_net_size = len(net.nodes)
+    min_path, max_path, cluster_size_ratios = None, None, []
+
+    for ratio_removed in tqdm(removal_rates, desc="- Instantaneous_attack", disable=not verbose, file=sys.stdout):
+        # Create deep copy of the original network
+        attacked_net = copy.deepcopy(net)
+        # Get list of nodes
+        nodes = list(attacked_net.nodes)
+
+        # Node connectivity is equal to the minimum number of nodes that must be removed to disconnect the node
+        connectivities = [(node, nx.node_connectivity(attacked_net, node)) for node in nodes]
+        connectivities.sort(key=lambda x: x[1], reverse=True)
+
+        num_removed_nodes = int(original_net_size * ratio_removed)
+        removed_nodes = [x[0] for x in connectivities[:num_removed_nodes]]
+
+        # Remove the nodes
+        attacked_net.remove_nodes_from(removed_nodes)
+
+        metrics = get_attack_metrics(attacked_net)
+
+        min_quantiles = mstats.mquantiles(metrics["shortest_paths"], axis=0)
+        max_quantiles = mstats.mquantiles(metrics["eccentricities"], axis=0)
+
+        # Replace 50% percentile with mean value
+        min_quantiles[1] = np.mean(metrics["shortest_paths"])
+        max_quantiles[1] = np.mean(metrics["eccentricities"])
+
+        min_path = np.vstack([min_path, min_quantiles]) if min_path is not None else min_quantiles
+        max_path = np.vstack([max_path, max_quantiles]) if max_path is not None else max_quantiles
+        cluster_size_ratios.append(metrics["cluster_sizes"])
+
+    return min_path, max_path, cluster_size_ratios
+
+
+def incremental_attack(net, removal_rates, verbose=False):
+    """
+    This type of attack simulates an instantaneous attack where at each step first remove the most connected node, and then
+    selecting and removing nodes in decreasing order of their connectivity k.
+    :param net: Network to attack
+    :param removal_rates: Vector of removal rates to test
+    :return: - shortest path [25% quantile, mean, 75% quantile]
+             - shortest path [25% quantile, mean, 75% quantile]
+             - cluster_sizes_ratios: Relative size of the clusters/islands of networks generated after the attack.
+    """
+
+    attacked_net = net
+    original_net_size = len(net.nodes)
+    min_path, max_path, cluster_size_ratios = None, None, []
+
+    for ratio_removed in tqdm(removal_rates, desc="- Instantaneous_attack", disable=not verbose, file=sys.stdout):
+        # Create deep copy of the original network
+        # Get list of nodes
+        nodes = list(attacked_net.nodes)
+
+        # Node connectivity is equal to the minimum number of nodes that must be removed to disconnect the node
+        connectivities = [(node, nx.node_connectivity(attacked_net, node)) for node in nodes]
+        connectivities.sort(key=lambda x: x[1], reverse=True)
+
+        num_removed_nodes = int(original_net_size * ratio_removed)
+        removed_nodes = [x[0] for x in connectivities[:num_removed_nodes]]
+
+        # Remove the nodes
+        attacked_net.remove_nodes_from(removed_nodes)
+
+        metrics = get_attack_metrics(attacked_net)
+
+        min_quantiles = mstats.mquantiles(metrics["shortest_paths"], axis=0)
+        max_quantiles = mstats.mquantiles(metrics["eccentricities"], axis=0)
+
+        # Replace 50% percentile with mean value
+        min_quantiles[1] = np.mean(metrics["shortest_paths"])
+        max_quantiles[1] = np.mean(metrics["eccentricities"])
+
+        min_path = np.vstack([min_path, min_quantiles]) if min_path is not None else min_quantiles
+        max_path = np.vstack([max_path, max_quantiles]) if max_path is not None else max_quantiles
+        cluster_size_ratios.append(metrics["cluster_sizes"])
+
+    return min_path, max_path, cluster_size_ratios
 
 
 
---
+++ utils/visualization.py
@@ -0,0 +1,121 @@
+import time
+import warnings
+
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+import numpy as np
+import powerlaw
+from scipy.stats import poisson
+
+from network_attacks import incremental_random_failure
+from utils.configuration_model import ConfigurationGenerator
+
+warnings.filterwarnings("ignore", category=UserWarning)
+
+
+def get_power_law_net(n_nodes, k, verbose=False):
+    distribution = powerlaw.Power_Law(xmin=1, parameters=[k])
+    degrees = distribution.generate_random(n_nodes).astype(np.int32)
+
+    start_time = time.time()
+    generator = ConfigurationGenerator(degrees)
+    net = generator.get_network()
+
+    if verbose:
+        print("** Gen PowerLaw k=%.2f took %.3fs" % (k, time.time() - start_time))
+
+    return net
+
+
+def get_poisson_net(n_nodes, mu, verbose=False):
+    distribution = poisson(mu)
+    degrees = distribution.rvs(size=n_nodes)
+
+    start_time = time.time()
+    generator = ConfigurationGenerator(degrees)
+    net = generator.get_network()
+
+    if verbose:
+        print("** Gen Poisson mu=%.2f took %.3fs" % (mu, time.time() - start_time))
+
+    return net
+
+
+def plot_metric_distribution(metrics_quantiles, ratios, y_label, labels, title=''):
+    fig, ax = plt.subplots(1, 1, figsize=(7, 5))
+
+    colors = cm.ocean(np.linspace(0.0, 0.7, len(metrics_quantiles)))
+
+    for quantiles, c in zip(metrics_quantiles, colors):
+        ax.plot(ratios, quantiles[:, 1], '-', color=c)
+        ax.fill_between(ratios, quantiles[:, 0], quantiles[:, 2], color=c, alpha=0.15)
+
+    ax.grid("on", alpha=0.1)
+    ax.set_ylabel(y_label)
+    ax.set_xlabel("Removal ratio")
+    ax.set_title(title)
+    if labels:
+        ax.legend(labels).set_zorder(10)
+
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_clustering_distribution(metrics_clusterings, ratios, y_label, labels, title=''):
+    fig, ax = plt.subplots(1, 1, figsize=(7, 5))
+
+    avg_size_isolated_clusters,  relative_size_largest_cluster = zip(*metrics_clusterings)
+    metrics_quantiles = [list(avg_size_isolated_clusters), list(relative_size_largest_cluster)]
+
+    colors = cm.ocean(np.linspace(0.0, 0.7, len(metrics_quantiles)))
+    markers = ["s", "o", "s", "o"]
+
+    for quantiles, c, marker in zip(metrics_quantiles, colors, markers):
+        ax.plot(ratios, quantiles, marker, fillstyle='none', color=c)
+
+
+    ax.grid("on", alpha=0.1)
+    ax.set_ylabel(y_label)
+    ax.set_xlabel("Removal ratio")
+    ax.set_title(title)
+    if labels:
+        ax.legend(labels).set_zorder(10)
+
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == "__main__":
+
+    exp_removal_ratios = np.linspace(0.0, 0.5, 10)
+    exp_num_nodes = [100]
+    exp_mus = [4]
+    exp_ks = [2.6]
+    exp_max_rate = 0.5
+    exp_removal_rate = 0.02
+    for n_nodes in exp_num_nodes:
+        for mu in exp_mus:
+            net = get_poisson_net(n_nodes=n_nodes, mu=mu, verbose=False)
+            attacked_net = net
+
+            min_path, max_path, cluster_size_ratios = incremental_random_failure(net=net,
+                                                                                 removal_rate=exp_removal_rate,
+                                                                                 max_rate=exp_max_rate,
+                                                                                 verbose=True)
+
+            print("Final cluster sizes:")
+            print(cluster_size_ratios[-1])
+
+            steps = int(exp_max_rate / exp_removal_rate)
+            title = "Random Attack - Poisson mu=%.2f " % (mu)
+            plot_metric_distribution([min_path, max_path],
+                                     np.cumsum([exp_removal_rate] * steps),
+                                     y_label="path length",
+                                     labels=[r'$d_{min}$', r'$d_{max}$'],
+                                     title=title)
+
+            plot_clustering_distribution(cluster_size_ratios,
+                                     np.cumsum([exp_removal_rate] * steps),
+                                     y_label="clusterings",
+                                     labels=[r'$S$', r'$\langle s \rangle$'],
+                                     title=title)
