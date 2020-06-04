457533d45ef1dfabe1443a53a031d64b4037f06e
jklimack <56316767+jklimack@users.noreply.github.com>
2020-06-04 22:33:19 +0200

    oregon added

---
+++ networks/oregon.py
@@ -0,0 +1,14 @@
+
+import networkx as nx
+import numpy as np
+
+def get_network():
+    filename = "networks/oregon2_010526.txt"
+    G = nx.read_edgelist(filename)
+    return G
+
+
+def degree_distribution(net):
+    degrees = np.array([x[1] for x in net.degree()])
+    counts = np.unique(degrees, return_counts=True)
+
