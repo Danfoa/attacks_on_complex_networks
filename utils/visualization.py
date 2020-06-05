298b1e847c01a45172b065addc8c29943e946e18
jklimack <56316767+jklimack@users.noreply.github.com>
2020-06-05 22:00:58 +0200

    adaptation to merge

--- utils/visualization.py
+++ utils/visualization.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 
+from utils.data_saver import load_results
+
 warnings.filterwarnings("ignore", category=UserWarning)
 
 
@@ -60,7 +62,10 @@ def plot_clustering_distribution(metrics_clusterings, ratios, y_label, labels, t
 def save_network_tracking(network_tracking, title, file_name):
     ratios = sorted(list(network_tracking.keys()))
     num_plots = len(ratios)
-
+    cols = int((num_plots + 1) / 2)
+    size = 5
+    plt.figure(figsize=(cols * size, 2 * size))
+    pos = nx.random_layout(network_tracking[ratios[0]])
     for i, ratio in enumerate(ratios):
         filename = os.path.join("results", file_name, "network_tracking({}).net".format(i))
         nx.write_pajek(network_tracking[ratio], filename)
@@ -98,7 +103,7 @@ def plot_comparisons_from_file_metrics(file_name, title, filenames, labels, exp_
         ax.legend(ylabels).set_zorder(10)
 
     plt.tight_layout()
-    file_name = os.path.join('..', 'results', file_name + "_dia_comparisons")
+    file_name = os.path.join('results', file_name + "_dia_comparisons")
     plt.savefig(file_name)
 
 
@@ -138,225 +143,6 @@ def plot_comparisons_from_file_clustering(file_name, title, filenames, labels, e
         ax.legend(ylabels).set_zorder(10)
 
     plt.tight_layout()
-    file_name = os.path.join('..', 'results', file_name+"_clust_comparisons")
+    file_name = os.path.join('results', file_name+"_clust_comparisons")
     plt.savefig(file_name)
 
-
-def incremental_attack_poisson(exp_removal_rate, exp_max_rate, exp_num_nodes, exp_mus, is_random_attack, track_net=1):
-    for n_nodes in exp_num_nodes:
-        for mu in exp_mus:
-            net = get_poisson_net(n_nodes=n_nodes, mu=mu, verbose=False)
-            if not is_random_attack:
-                file_name = "poisson-incr-attack-mu=%.2f-n_nodes=%d" % (mu, n_nodes)
-                title = "Incremental Attack - Poisson nodes=%d mu=%.2f " % (n_nodes, mu)
-
-                min_path, max_path, cluster_size_ratios, network_tracking = incremental_attack(net=net,
-                                                                                               removal_rate=exp_removal_rate,
-                                                                                               max_rate=exp_max_rate,
-                                                                                               verbose=True,
-                                                                                               track_net_num=track_net)
-
-            else:
-                min_path, max_path, cluster_size_ratios, network_tracking = incremental_random_failure(net=net,
-                                                                                                       removal_rate=exp_removal_rate,
-                                                                                                       max_rate=exp_max_rate,
-                                                                                                       track_net_num=track_net)
-
-                file_name = "poisson-incr-failure-mu=%.2f-n_nodes=%d" % (mu, n_nodes)
-                title = "Incremental Failure - Poisson nodes=%d mu=%.2f " % (n_nodes, mu)
-            save_results(min_path, max_path, cluster_size_ratios, file_name)
-            plot_network_tracking(network_tracking, title, file_name)
-            steps = int(exp_max_rate / exp_removal_rate)
-            plot_metric_distribution([min_path, max_path],
-                                     np.cumsum([exp_removal_rate] * steps),
-                                     y_label="path length",
-                                     labels=[r'$d_{min}$', r'$d_{max}$'],
-                                     title=title,
-                                     filename=file_name + '_distr.png')
-
-            clusters_info = [x[0] for x in cluster_size_ratios]
-            plot_clustering_distribution(clusters_info,
-                                         np.cumsum([exp_removal_rate] * steps),
-                                         y_label="clusterings",
-                                         labels=[r'$S$', r'$\langle s \rangle$'],
-                                         title=title,
-                                         filename=file_name + '_clust.png')
-
-
-def instantaneous_attack_poisson(exp_num_nodes, exp_removal_ratios, exp_mus, is_random_attack):
-    for n_nodes in exp_num_nodes:
-        for mu in exp_mus:
-            net = get_poisson_net(n_nodes=n_nodes, mu=mu, verbose=True)
-
-            if not is_random_attack:
-                min_path, max_path, cluster_size_ratios = instantaneous_attack(net=net, removal_rates=exp_removal_ratios, verbose=True)
-
-                file_name = "poisson-inst-attack-mu=%.2f-n_nodes=%d" % (mu, n_nodes)
-                title = "Instantaneous Attack - Poisson nodes=%d mu=%.2f " % (n_nodes, mu)
-
-            else:
-                min_path, max_path, cluster_size_ratios = instantaneous_random_failure(net=net,
-                                                                               removal_rates=exp_removal_ratios,
-                                                                               verbose=True)
-
-                file_name = "poisson-inst-failure-mu=%.2f-n_nodes=%d" % (mu, n_nodes)
-                title = "Instantaneous Failure - Poisson nodes=%d mu=%.2f " % (n_nodes, mu)
-            save_results(min_path, max_path, cluster_size_ratios, file_name)
-
-            plot_metric_distribution([min_path, max_path],
-                                     exp_removal_ratios,
-                                     y_label="path length",
-                                     labels=[r'$d_{min}$', r'$d_{max}$'],
-                                     title=title,
-                                     filename=file_name + '_distr.png')
-            clusters_info = [x[0] for x in cluster_size_ratios]
-            plot_clustering_distribution(clusters_info,
-                                         exp_removal_ratios,
-                                         y_label="clusterings",
-                                         labels=[r'$S$', r'$\langle s \rangle$'],
-                                         title=title,
-                                         filename=file_name + '_clust.png')
-
-
-def incremental_attack_powerlaw(exp_removal_rate, exp_max_rate, exp_num_nodes, exp_ks, is_random_attack, track_net=1):
-    for n_nodes in exp_num_nodes:
-        for k in exp_ks:
-            net = get_power_law_net(n_nodes, k, verbose=False)
-            if not is_random_attack:
-                min_path, max_path, cluster_size_ratios, network_tracking = incremental_attack(net=net,
-                                                                                               removal_rate=exp_removal_rate,
-                                                                                               max_rate=exp_max_rate,
-                                                                                               track_net_num=track_net)
-
-                file_name = "powerlaw-incr-attack-k=%.2f-n_nodes=%d" % (k, n_nodes)
-                title = "Incremental Attack - Powerlaw nodes=%d k=%.2f " % (n_nodes, k)
-            else:
-                min_path, max_path, cluster_size_ratios, network_tracking = incremental_random_failure(net=net,
-                                                                                                       removal_rate=exp_removal_rate,
-                                                                                                       max_rate=exp_max_rate,
-                                                                                                       track_net_num=track_net)
-
-                file_name = "powerlaw-incr-failure-k=%.2f-n_nodes=%d" % (k, n_nodes)
-                title = "Incremental Failure - Powerlaw nodes=%d k=%.2f " % (n_nodes, k)
-
-            save_results(min_path, max_path, cluster_size_ratios, file_name)
-            plot_network_tracking(network_tracking, title, file_name)
-            steps = int(exp_max_rate / exp_removal_rate)
-
-            plot_metric_distribution([min_path, max_path],
-                                     np.cumsum([exp_removal_rate] * steps),
-                                     y_label="path length",
-                                     labels=[r'$d_{min}$', r'$d_{max}$'],
-                                     title=title,
-                                     filename=file_name + '_distr.png')
-
-            clusters_info = [x[0] for x in cluster_size_ratios]
-            plot_clustering_distribution(clusters_info,
-                                         np.cumsum([exp_removal_rate] * steps),
-                                         y_label="clusterings",
-                                         labels=[r'$S$', r'$\langle s \rangle$'],
-                                         title=title,
-                                         filename=file_name + '_clust.png')
-
-
-def instantaneous_attack_powerlaw(exp_num_nodes, exp_removal_ratios, exp_ks, is_random_attack):
-    for n_nodes in exp_num_nodes:
-        for k in exp_ks:
-            net = get_power_law_net(n_nodes, k, verbose=False)
-            if not is_random_attack:
-                min_path, max_path, cluster_size_ratios = instantaneous_attack(net=net, removal_rates=exp_removal_ratios,
-                                                                               verbose=True)
-
-                file_name = "powerlaw-inst-attack-k=%.2f-n_nodes=%d" % (k, n_nodes)
-                title = "Instantaneous Attack - Powerlaw nodes=%d k=%.2f " % (n_nodes, k)
-            else:
-                min_path, max_path, cluster_size_ratios = instantaneous_random_failure(net=net,
-                                                                               removal_rates=exp_removal_ratios,
-                                                                               verbose=True)
-
-                file_name = "powerlaw-inst-failure-k=%.2f-n_nodes=%d" % (k, n_nodes)
-                title = "Instantaneous Failure - Powerlaw nodes=%d k=%.2f " % (n_nodes, k)
-
-            save_results(min_path, max_path, cluster_size_ratios, file_name)
-            plot_metric_distribution([min_path, max_path],
-                                     exp_removal_ratios,
-                                     y_label="path length",
-                                     labels=[r'$d_{min}$', r'$d_{max}$'],
-                                     title=title,
-                                     filename=file_name + '_distr.png')
-            clusters_info = [x[0] for x in cluster_size_ratios]
-            plot_clustering_distribution(clusters_info,
-                                         exp_removal_ratios,
-                                         y_label="clusterings",
-                                         labels=[r'$S$', r'$\langle s \rangle$'],
-                                         title=title,
-                                         filename=file_name + '_clust.png')
-
-
-def incremental_attack_(net, file_name, title, exp_removal_rate, exp_max_rate, is_random_attack, track_net=1):
-    if not is_random_attack:
-        min_path, max_path, cluster_size_ratios, network_tracking = incremental_attack(net=net,
-                                                                                       removal_rate=exp_removal_rate,
-                                                                                       max_rate=exp_max_rate,
-                                                                                       verbose=True,
-                                                                                       track_net_num=track_net)
-    else:
-        min_path, max_path, cluster_size_ratios, network_tracking = incremental_random_failure(net=net,
-                                                                                               removal_rate=exp_removal_rate,
-                                                                                               max_rate=exp_max_rate,
-                                                                                               track_net_num=track_net,
-                                                                                               verbose=True)
-    save_results(min_path, max_path, cluster_size_ratios, file_name)
-    plot_network_tracking(network_tracking, title, file_name)
-    steps = int(exp_max_rate / exp_removal_rate)
-    plot_metric_distribution([min_path, max_path],
-                             np.cumsum([exp_removal_rate] * steps),
-                             y_label="path length",
-                             labels=[r'$d_{min}$', r'$d_{max}$'],
-                             title=title,
-                             filename=file_name + '_distr.png')
-
-    clusters_info = [x[0] for x in cluster_size_ratios]
-    plot_clustering_distribution(clusters_info,
-                                 np.cumsum([exp_removal_rate] * steps),
-                                 y_label="clusterings",
-                                 labels=[r'$S$', r'$\langle s \rangle$'],
-                                 title=title,
-                                 filename=file_name + '_clust.png')
-
-
-if __name__ == "__main__":
-    exp_removal_rate = 0.0025
-    exp_removal_ratios = np.linspace(0.0, 0.5, 10)
-
-    exp_max_rate = 0.05
-    # exp_num_nodes = [2000]
-    exp_num_nodes = [100]  # Test the attacks with different sizes of networks
-    exp_mus = [4, 6, 8]
-    exp_ks = [2.6]
-
-    #for is_random_attack in [False]:
-        # Poisson
-        #incremental_attack_poisson(exp_removal_rate, exp_max_rate, exp_num_nodes, exp_mus, is_random_attack, track_net=7)
-        #instantaneous_attack_poisson(exp_num_nodes, exp_removal_ratios, exp_mus, is_random_attack)
-
-        # Scale Free
-        #incremental_attack_powerlaw(exp_removal_rate, exp_max_rate, exp_num_nodes, exp_ks, is_random_attack, track_net=7)
-        #instantaneous_attack_powerlaw(exp_num_nodes, exp_removal_ratios, exp_ks, is_random_attack)
-
-        # Oregon
-        #net = oregon.get_network()
-        #file_name = title = "GNutella_incr_fail"
-        #oregon.degree_distribution(net)
-        #incremental_attack_(net, file_name, title, exp_removal_rate, exp_max_rate, is_random_attack, track_net=8)
-
-
-    # mu comparisons
-    filenames = ["poisson-incr-attack-mu=4.00-n_nodes=100",
-                  "poisson-incr-attack-mu=6.00-n_nodes=100",
-                  "poisson-incr-attack-mu=8.00-n_nodes=100"]
-    filename = title = "mu_comparisons_poisson-incr-attack"
-    plot_comparisons_from_file_clustering(filename, title, filenames, ["mu={}".format(mu) for mu in exp_mus],
-                                          exp_max_rate, exp_removal_rate)
-    plot_comparisons_from_file_metrics(filename, title, filenames, ["mu={}".format(mu) for mu in exp_mus],
-                                          exp_max_rate, exp_removal_rate)
